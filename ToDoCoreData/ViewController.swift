0736c36f524b81672bb8a02c5d9a14f79775c523
Bogdan <bgdnbskkv@gmail.com>
2019-09-03 19:53:34 +0500

    Initial Commit

---
+++ ToDoCoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoCoreData
+//
+//  Created by Bogdan Baskakov on 03/09/2019.
+//  Copyright © 2019 Bogdan Baskakov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
